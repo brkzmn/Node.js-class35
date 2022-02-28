ddea6d84cb6666ef7bcdfb7d83a363a53a6391c6
Burak Özmen <brkzmn.wd@gmail.com>
2022-03-01 00:27:05 +0100

    modified

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,5 +1,23 @@
-import * as express from "express";
+import express from "express";
+import fetch from "node-fetch";
+import exphbs from "express-handlebars";
+import path from "path";
 
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello from backend to frontend!");
+});
+
+app.post("/weather", (req, res) => {
+  console.log(req.body, "request");
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
