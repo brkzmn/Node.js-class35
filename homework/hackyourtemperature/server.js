157f1ea3340f8b1237b52196c6ca0a3584b113e1
Burak Özmen <brkzmn.wd@gmail.com>
2022-03-07 12:17:49 +0100

    added fetch

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 import exphbs from "express-handlebars";
 import path from "path";
+import { keys } from "./sources/keys.js";
 
 const app = express();
 app.use(express.json());
@@ -10,10 +11,27 @@ app.get("/", (req, res) => {
   res.send("hello from backend to frontend!");
 });
 
-app.post("/weather", (req, res) => {
-  console.log(req.body, "request");
+app.post("/weather", async (req, res) => {
   const cityName = req.body.city;
-  res.send(cityName);
+  // res.send(cityName);
+  try {
+    const fetchResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${keys.API_KEY}`
+    );
+    if (fetchResponse.ok) {
+      const responseJson = await fetchResponse.json();
+      const temperature = responseJson.main.temp;
+      console.log(`The temperature in ${cityName} is ${temperature} °C!`);
+      res.json({
+        weatherText: `The temperature in ${cityName} is ${temperature} °C!`,
+      });
+    } else {
+      res.json({ weatherText: "City is not found!" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ weatherText: `${error.message}` });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,1 @@
+export const keys = { API_KEY: "68b7cb8df654d99aa72ed9a77bd90cad" };
